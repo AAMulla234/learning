520d24cbee8b0f6f1d0c46065771ecd0b6edfda1
AAMulla234 <53477084+AAMulla234@users.noreply.github.com>
2021-05-17 14:23:46 +0530

    Create ExampleUsingJava9.java

---
+++ ExampleUsingJava9.java
@@ -0,0 +1,24 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+/**
+ * This class will explain enhancement of try with resources block which was
+ * introduced with JDK9 .
+ *
+ * @author akhtar.jahan.qureshi@gmail.com
+ *
+ */
+public class ExmapleUsingJava9 {
+
+       public static void main(String[] args) throws FileNotFoundException {
+             FileOutputStream fileStream = new FileOutputStream("App.log");
+             try (fileStream) {
+                    String logMessage = "[INFO] DATA NEED TO BE WRITTEN";
+                    byte logMessageInBytes[] = logMessage.getBytes();
+                    fileStream.write(logMessageInBytes);
+                    System.out.println("Updated log files.");
+             } catch (Exception e) {
+                    // handle error
+             }
+       }
+}
